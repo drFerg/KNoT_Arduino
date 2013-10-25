6e19bbd288c3274f6c67be2b0d48c192212746f1
Fergus <fergus.leahy@gmail.com>
2013-10-25 15:09:24 +0100

    Fixed various cosmetic bugs in timer code

--- arduino/libs/KNoT/callback_timer.cpp
+++ arduino/libs/KNoT/callback_timer.cpp
@@ -10,8 +10,8 @@
 #define MAX_TIME_MICRO_S (MAX_TIME_S * ONE_SECOND)
 
 /* Helper function macros */
-#define enable_interrupts sei()
-#define disable_interrupts cli()
+#define enable_interrupts() sei()
+#define disable_interrupts() cli()
 #define has_timer_expired(time) (time == 0)
 #define has_time_left(time) (time > 0)
 #define is_timer_free(time) (time == -1)
@@ -19,7 +19,7 @@
 typedef struct timer {
 	int timer;
 	int value;
-	void *callback(int);
+	void (*callback)(int);
 }Timer;
 
 static Timer timers[NUM_OF_TIMERS];
@@ -37,13 +37,13 @@ void timer_ISR() {
 	timer_iter = 0;
 	next_timer_len = MAX_TIME_MICRO_S;
 	for (int i = 0; i < NUM_OF_TIMERS; i++) {
-		if (has_time_left(timers[i]->timer)){ /* Check if a valid timer */
-			timers[i]->timer = timers[i]->timer - elapsed; /* Decrement time elapsed */
-			if (has_timer_expired(timers[i]->timer)) { /* Check if now expired */
+		if (has_time_left(timers[i].timer)){ /* Check if a valid timer */
+			timers[i].timer = timers[i].timer - elapsed; /* Decrement time elapsed */
+			if (has_timer_expired(timers[i].timer)) { /* Check if now expired */
 				expired_timers++;
 			}
-			else if (timers[i]->timer < next_timer_len) { /* find next closest time */
-				next_timer_len = timers[i]->timer;
+			else if (timers[i].timer < next_timer_len) { /* find next closest time */
+				next_timer_len = timers[i].timer;
 			}
 		}
 	}
@@ -54,7 +54,7 @@ void timer_ISR() {
 void init_timer() {
 	pinMode(4, OUTPUT); /* Pin 4 is used for timer interrupt */
 	for (int i = 0; i < NUM_OF_TIMERS; i++) {
-		timers[i]->timer = UNSET_TIMER;
+		timers[i].timer = UNSET_TIMER;
 	}
 	Timer1.initialize(MAX_TIME_MICRO_S);
 	Timer1.attachInterrupt(timer_ISR);
@@ -62,12 +62,12 @@ void init_timer() {
 
 int set_timer(double timer, int func_value, void (*callback)(int)) {
 	for (int i = 0; i < NUM_OF_TIMERS; i++) {
-		if (is_timer_free(timers[i]->timer)){
-			timers[i]->timer = timer * ONE_SECOND; /* Timer is in microseconds */
-			timers[i]->value = func_value; /* Store value to pass to callback func */
-			timers[i]->callback = callback;
-			if (timers[i]->timer < next_timer_len) { 
-				next_timer_len = timers[i]->timer; /* Check if less than next timer */
+		if (is_timer_free(timers[i].timer)){
+			timers[i].timer = timer * ONE_SECOND; /* Timer is in microseconds */
+			timers[i].value = func_value; /* Store value to pass to callback func */
+			timers[i].callback = callback;
+			if (timers[i].timer < next_timer_len) { 
+				next_timer_len = timers[i].timer; /* Check if less than next timer */
 				elapsed = next_timer_len;
 				Timer1.setPeriod(next_timer_len);
 			}
@@ -79,7 +79,7 @@ int set_timer(double timer, int func_value, void (*callback)(int)) {
 
 void remove_timer(int timer_id) {
 	if (timer_id < NUM_OF_TIMERS) {
-		timers[timer_id]->timer = UNSET_TIMER;
+		timers[timer_id].timer = UNSET_TIMER;
 	}
 }
 
@@ -90,13 +90,13 @@ int timer_expired() {
 int run_next_expired_timer() {
 	disable_interrupts();
 	for (;timer_iter < NUM_OF_TIMERS; timer_iter++) {
-		if (has_timer_expired(timers[timer_iter]->timer)) {
+		if (has_timer_expired(timers[timer_iter].timer)) {
 			int to_run = timer_iter; /* Save timer incase of later interrupt */
-			timers[timer_iter]->timer = UNSET_TIMER;
+			timers[timer_iter].timer = UNSET_TIMER;
 			expired_timers--;
 			enable_interrupts(); 
 			/* Enable interrupts from now incase of long callback func */
-			timers[to_run]->callback(timers[to_run]->value);
+			timers[to_run].callback(timers[to_run].value);
 			return 1;
 		}
 	}
@@ -106,6 +106,6 @@ int run_next_expired_timer() {
 
 void run_all_expired_timers() {
 	while (expired_timers) {
-		run_next_expired();
+		run_next_expired_timer();
 	}
 }
